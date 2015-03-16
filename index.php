1e60832b66eb41ae5ef6faaae543956693f4588a
Jonathan Stempel <jman.go.mets@gmail.com>
2015-03-16 19:50:46 -0400

    placeholder

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>My Film Critic</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+</head>
+<body>
+
+<div class="container">
+	<?php require_once("includes/header.php"); ?>
+	<article>
+		<h2>Homepage</h2>
+		<br />
+		<br />
+		If you don't have an account click <a href="signup.php">here</a>.
+		<br /><br />
+	</article>
+</div>
+
+<?php require_once("includes/footer.php"); ?>
+
+</body>
+</html>
