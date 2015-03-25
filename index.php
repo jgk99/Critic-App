ecfa5e2367f522b63efc5dbd3aa8e652663473f4
RenRokB <robklay@gmail.com>
2015-03-24 21:47:58 -0400

    Fixed conflict

--- index.php
+++ index.php
@@ -17,19 +17,9 @@
 	<?php require_once("includes/header.php"); ?> 
 
 	<div class="col-md-8">
-<<<<<<< HEAD
-<h2> Our Mission</h2>
-<p>Imagine it's a saturday, you and some friends want to get together and watch a movie. It's all great until you can't decide what movie you want to see. Someone suggests a movie and you're all excited, but you go on a website like rotten tomatoes and see that it has a terrible score. You don't know why it's bad, but you just assume it is because of the low score. You might want to see it anyways, but your friends are scared off by the low score, and rightly so. Maybe you would have all loved the movie despite what the score says, but there's always a chance that you just wasted two hours that you'll never get back. <br                                    /> Society has come up with some ways to partially get around this problem though. Critics can provide a better view into movies, and more in depth reviews past a score. But this again poses its own problem: Critics are people, and people have opinions. Do their opinions match yours?</p> <p>
-
-Society does have some ways to partially get around this problem though. People read what critics say about the movie online before they watch it. This allows people to be able to tell when there is a great movie that everyone loves and be able to tell when there is horrible movie that everyone hates. The problem is that people have different opinions and one person may hate a movie that another person loves and vice versa. That leaves conflicting reviews from the critics,<strong> how will you know which critic to believe?</strong> </p> <p>
-
-	The answer lies in our website, My Movie Critic. My Movie Critic eliminates the problem of which critic to believe. First our website has you rate a few movies that you have already seen so that we can better get to know you and your taste in movies. When you look up movies from then on, you can see reviews of critics who have similar tastes to you, greatly increasing your chance of a positive movie watching experience.</p>
-</div><br />
-=======
 		<h2> Our Mission</h2>
 		<p>
 			Let's say it's a saturday, you're with your pals and you got all day to do whatever you want. So someone comes up with the idea to see a movie. Then, you remember there is a movie you want to see, maybe it starred Liam Neeson or maybe it didn't. You then suggest that movie and everyone is on board. However, one of your pals decides to look up it's score on rotten tomatoes. There is that dreaded moment when the movie's score is a 60. You don't know why it's bad, you just assume it is because of it's low score. You love Liam Neeson and all of his movies. Now, no one wants to see the movie.   That's where we come in. <br /> People go to movies to relax and enjoy themselves so that they can be distracted from the real world. Movies are one of the greatest things on earth, but there is one problem with them; <strong>it is extremely hard to choose which movie to watch</strong>, if you choose a good one you are plainly lucky, and if you choose a bad one you just wasted 2 hours of your short life.</p> <p>
->>>>>>> origin/master
 
 			Society does have some ways to partially get around this problem though. People read what critics say about the movie online before they watch it. This allows people to be able to tell when there is a great movie that everyone loves and be able to tell when there is horrible movie that everyone hates. The problem is that people have different opinions and one person may hate a movie that another person loves and vice versa. That leaves conflicting reviews from the critics,<strong> how will you know which critic to believe?</strong> </p> <p>
 
