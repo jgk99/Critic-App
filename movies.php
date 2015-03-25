2b42e002849b19ef271621339c4fc2651ff9ea91
RenRokB <robklay@gmail.com>
2015-03-24 21:44:24 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit de3b9f62049f9b407ebc7e85f86c4a59d8b7044d.

--- movies.php
+++ movies.php
@@ -1,10 +1,10 @@
 <?php
 
-			$box_office = file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=nqr68qe538tc2hsub2c5fubt');
-			$movielist = json_decode($box_office);
-			$total = $movielist->movies;
+$box_office = file_get_contents('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=nqr68qe538tc2hsub2c5fubt');
+$movielist = json_decode($box_office);
+$total = $movielist->movies;
 
-			for( )
+//for( )
 
 ?>
 
@@ -21,33 +21,36 @@
 
 <div class="container">
 	<?php require_once("includes/header.php"); ?>
+	
 	<div class="jumbotron">
-  		<h2>Welcome To My Movie Critic</h2>
-  		<p>To learn more about our service please visit our Mission page.</p>
-  		<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+		<h2>Welcome To My Movie Critic</h2>
+		<p>To learn more about our service please visit our Mission page.</p>
+		<p><a class="btn btn-primary btn-lg" href="/" role="button">Learn more</a></p>
 	</div>
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="..." alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
-    </div>
-  </div>
-<div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="..." alt="...">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>...</p>
-        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-      </div>
-    </div>
-  </div>
-</div>
+	
+	<div class="row">
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php require_once("includes/footer.php"); ?>
