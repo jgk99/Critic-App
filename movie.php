8cdf366d30d1a5f3eaeef9ec9dcd48341c9d19d8
RenRokB <robklay@gmail.com>
2015-03-26 13:14:47 -0400

    Added stars to movie page

--- movie.php
+++ movie.php
@@ -16,9 +16,11 @@ if (isset($_GET['id'])) {
 <head>
 	<title>My Movie Critic</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/ratings.css">
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/ratings.js"></script>
 </head>
 <body>
 
@@ -58,6 +60,13 @@ if (isset($_GET['id'])) {
 		<script type="text/javascript">
 			document.write("<img src=\"http://img.omdbapi.com/?apikey=51ced2f&i=tt" + movie.alternate_ids['imdb'] + "\" width=\"\"/ class='img-responsive' />");
 		</script>
+		<div class="rating">
+			<span class="star"><input type="radio" name="star" id="star5" value="5"><label for="star5"></label></span>
+			<span class="star"><input type="radio" name="star" id="star4" value="4"><label for="star4"></label></span>
+			<span class="star"><input type="radio" name="star" id="star3" value="3"><label for="star3"></label></span>
+			<span class="star"><input type="radio" name="star" id="star2" value="2"><label for="star2"></label></span>
+			<span class="star"><input type="radio" name="star" id="star1" value="1"><label for="star1"></label></span>
+		</div>
 	</div>
 
 	<div class ="col-md-9 col-md-offset-0">
