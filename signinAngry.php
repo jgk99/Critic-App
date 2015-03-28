c3748daf393e3f0a75704d294dcc0bfcc04cd438
RenRokB <robklay@gmail.com>
2015-03-28 02:36:04 -0400

    More https

--- signinAngry.php
+++ signinAngry.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])) {
 	<title>My Movie Critic</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<meta charset="UTF-8">
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
