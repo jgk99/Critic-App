e5e0cc8514030910249171572876664b3c8c15c7
Jonathan Stempel <jman.go.mets@gmail.com>
2015-03-29 20:14:29 -0400

    Fix

--- index.php
+++ index.php
@@ -67,15 +67,15 @@ else{
 					}
 				}
 			} else {
-				$rate_more_movies = true;
+				$rate_more_movies = "";
 				echo '.' . count($matches) . '.';
 			}
 		} else {
-			$rate_more_movies = true;
+			$rate_more_movies = "";
 		}
 
 		echo '<script type ="text/javascript">
-				if ("$rate_more_movies" == "1") {
+				if ("'.$rate_more_movies.'" == "") {
 					document.write("Rate more movies to find some similar critics.<br /><br /><br /><br /><br /><br /><br /><br />");
 				} else {
 					document.write("<h2>Your Top Critics:</h2>");
