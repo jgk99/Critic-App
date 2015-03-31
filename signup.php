e9db066dd6dca6958a9da4a90633fce87ac5e53b
Jonathan Stempel <jman.go.mets@gmail.com>
2015-03-31 16:40:48 -0400

    More fixing

--- signup.php
+++ signup.php
@@ -98,7 +98,7 @@ if (isset($_POST['submit'])) {
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </head>
 <body>
 
