3014aa460bb317b522490f219e68754f2b301de9
jmanyc <jman.go.mets@gmail.com>
2015-04-01 10:57:59 -0400

    IE Redirect

--- includes/forcessl.php
+++ includes/forcessl.php
@@ -13,6 +13,23 @@ else if ($_SERVER["HTTPS"] != "on")
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     exit();
 }
+
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
+   echo 'Internet explorer';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
+    echo 'Internet explorer';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
+   echo 'Mozilla Firefox';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
+   echo 'Google Chrome';
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
+   echo "Opera Mini";
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
+   echo "Opera";
+ elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
+   echo "Safari";
+ else
+   echo 'Something else';
 /*if(isset($_SESSION["id"])) {
 
 
