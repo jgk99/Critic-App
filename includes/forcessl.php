f27c676704b01cb4cf1402b397ac106e4c7238c7
jmanyc <jman.go.mets@gmail.com>
2015-04-01 11:07:52 -0400

    IE Redirect Complete

--- includes/forcessl.php
+++ includes/forcessl.php
@@ -14,22 +14,11 @@ else if ($_SERVER["HTTPS"] != "on")
     exit();
 }
 
-if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
-   echo 'Internet explorer';
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
-    echo 'Internet explorer';
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
-   echo 'Mozilla Firefox';
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
-   echo 'Google Chrome';
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
-   echo "Opera Mini";
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
-   echo "Opera";
- elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
-   echo "Safari";
- else
-   echo 'Something else';
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE || strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE)
+{
+     header("Location: https://www.google.com/chrome/browser/desktop/");
+     exit();
+}
 /*if(isset($_SESSION["id"])) {
 
 
